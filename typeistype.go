6b5f6a6a65c73be01eb7fa1e8293b83a8efddbd7
Oleg Subachev <git@solsw.ru>
2023-03-21 15:03:41 +0500

    TypeIsType modified

--- typeistype.go
+++ typeistype.go
@@ -17,20 +17,24 @@ import (
 // [type assertion]: https://go.dev/ref/spec#Type_assertions
 // [implements]: https://go.dev/ref/spec#Interface_types
 func TypeIsType[T, O any]() bool {
-	oType := reflect.TypeOf((*O)(nil)).Elem()
 	var t0 any = ZeroValue[T]()
-	if t0 != nil {
+	if t0 == nil {
+		// t0 is nil for pointers, functions, interfaces, slices, channels, and maps
+		oType := reflect.TypeOf((*O)(nil)).Elem()
+		if oType.Kind() == reflect.Interface {
+			tType := reflect.TypeOf((*T)(nil)).Elem()
+			return tType.Implements(oType)
+		}
+	} else {
+		_, isO := t0.(O)
+		if isO {
+			return true
+		}
 		tValue := reflect.ValueOf(t0)
+		oType := reflect.TypeOf((*O)(nil)).Elem()
 		if tValue.CanConvert(oType) {
 			return true
 		}
-		_, isO := t0.(O)
-		return isO
-	}
-	// t0 is nil for pointers, functions, interfaces, slices, channels, and maps
-	if oType.Kind() == reflect.Interface {
-		tType := reflect.TypeOf((*T)(nil)).Elem()
-		return tType.Implements(oType)
 	}
 	return false
 }
